644e916044439ab4fd774c08c58255e35086ae32
Josebautista10 <cooldj1020@gmail.com>
2021-11-23 11:29:03 -0700

    finish useState greeting with extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,21 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting(props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(props.initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    const value = event.target.value
+    setName(value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        {/* Set the value to name so user can still type */}
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +25,8 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  // let greeting accept a prop so we can display it right away and use it for state
+  return <Greeting initialName={""}/>
 }
 
 export default App
